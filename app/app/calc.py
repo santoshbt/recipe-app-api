d8d148ea0893d59122975351767ae6ad01070e20
llssbb <bt.santosh01@gmail.com>
2024-01-09 22:51:59 +0000

    linting and test fixes

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
 """
 Calculator Functions
 """
-    
+
+
 def add(x, y):
     """ Add  x and y and return the result"""
     return x + y
+
 
 def subtract(x, y):
     """ subtract x from y and return the result"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,22 +1,22 @@
 """
 Sample Tests
 """
-
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test with Cal Module"""
-    
+
     def test_add_numbers(self):
         """ Test adding numbers together. """
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
-        
+
     def test_subtract_numbers(self):
         """ Test subtract numbers """
         res = calc.subtract(15, 10)
-        
+
         self.assertEqual(res, 5)
